73fa2e7923131189dcdfa7b5bbc9847f6b65dfdc
Kenji Pa <limouren@gmail.com>
2022-12-29 15:02:03 +0800

    limouren: Set up development environment

---
+++ limouren/shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import
+    (fetchTarball {
+      name = "nixpkgs";
+      url = "https://github.com/NixOS/nixpkgs/archive/37c2766.tar.gz";
+      sha256 = "0my1hkwnihnpfk0mf61b7vzbd5kfzyprq9xmsv5jdd274jp0y2zs";
+    })
+    {
+      config.allowBroken = true;
+    }
+}:
+
+pkgs.mkShell {
+  packages = [
+    pkgs.elixir_1_14
+    pkgs.postgresql_15
+  ];
+}
