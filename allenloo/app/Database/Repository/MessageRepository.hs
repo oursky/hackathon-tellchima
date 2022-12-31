7d1c1f582e261f0e1c1a0eb335f1c6b4377a18de
axolotl0212 <allenl0212@gmail.com>
2022-12-31 08:05:21 +0800

    allenloo: Add mark message published query

--- allenloo/app/Database/Repository/MessageRepository.hs
+++ allenloo/app/Database/Repository/MessageRepository.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Database.Repository.MessageRepository where
 
@@ -9,6 +10,7 @@ import Data.ByteString.UTF8 (fromString)
 import Data.Int
 import Database.Entity.Message (MessageEntity)
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.SqlQQ (sql)
 import GHC.Generics
 import Model.AppConfig (AppConfig, dbConnectionStr)
 
@@ -38,3 +40,20 @@ createMessage appConfig message = do
       (execute dbConn "INSERT INTO messages (text, published) VALUES (?, ?)" message)
   close dbConn
   return insertedCount
+
+markMessagesPublished :: AppConfig -> [Int] -> IO Int64
+markMessagesPublished appConfig ids = do
+  dbConn <- connectPostgreSQL $ fromString $ dbConnectionStr appConfig
+  let query =
+        [sql|
+          UPDATE messages
+            SET published = TRUE
+          FROM (VALUES (?)) as update(id)
+          WHERE messages.id = update.id
+        |]
+  updatedCount <-
+    withTransaction
+      dbConn
+      (executeMany dbConn query (map Only ids))
+  close dbConn
+  return updatedCount
